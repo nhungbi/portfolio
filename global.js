3fd54d82aed06659e65648cc247121011dee99fb
nhungbi <nhungvan16@gmail.com>
2024-02-23 12:17:41 -0500

    testing links

--- global.js
+++ global.js
@@ -33,16 +33,22 @@ const ARE_WE_HOME = document.documentElement.classList.contains("home");
 for (let url in pages) {
 	let title = pages[url];
 
-	if (location.host !== "127.0.0.1:5500") { // if on hosted, then need "/portfolio"
-		url = "portfolio/" + url
-	}
-	
+
 	if (!ARE_WE_HOME && !url.startsWith("http")) {
 		console.log('not home')
 		url = "../" + url;
 	}
+
+	if (location.host !== "127.0.0.1:5500") { // if on hosted, then need "/portfolio"
+		url = "portfolio/" + url
+	}
+
+	if (!ARE_WE_HOME && location.host !== "127.0.0.1:5500") { // if not on hosted
+		console.log('not home 1')
+		url = "../" + url
+	}
 	// url = ARE_WE_HOME && !url.startsWith("http") ? url : "../" + url;
-	console.log(location)
+	console.log(location, url)
 
 	let a = document.createElement("a");
 	a.href = url;
